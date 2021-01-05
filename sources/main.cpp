ad7f15d38df2da9b2e0fdd7596514f2b9022a29b
irrengar <43902864+irrengar@users.noreply.github.com>
2021-01-05 11:51:57 +0330

    Update main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,27 +1,84 @@
-#include <iostream>
+
+
 #include <string>
+#include <iostream>
+#include <ctime>
 #include <fstream>
+#include <map>
+#include "Event.h"
+#include "Menu.h"
 
 using namespace std;
 
-class MySingleton{
-public:
-    static MySingleton* Instance;
-    static MySingleton* getInstance();
+int main() {
+    typedef multimap<string, Event> mmap;
+    typedef pair<string, Event> mmap_pair;
 
-private:
-    MySingleton();
-};
-MySingleton* MySingleton::Instance= NULL;
-MySingleton::MySingleton() {
-    cout<<"in Constractor.."<<endl;
-}
-MySingleton* MySingleton::getInstance() {
-    if(Instance==NULL)
-        {
-        Instance = new MySingleton();
+    int thisMonth, thisDay, thisYear;
+    //int menuChoice;
+
+    char title[256], description[256], done[256];
+    char hour[256], minute[256], month[256], day[256], year[256];
+
+    mmap container;
+
+    ifstream fin;
+    ofstream fout;
+
+    cout << "Welcome to REMINDER!" << endl;
+    cout << "What is today's day?" << endl;
+    cout << "Today's Month: ";
+    cin >> thisMonth;
+    cout << "\nToday's Day: ";
+    cin >> thisDay;
+    cout << "\nToday's Year: ";
+    cin >> thisYear;
+
+    fin.open("savedEvents.txt");
+
+    if (!fin.is_open()) {
+        cout << "NO FILE FOUND!" << endl;
+    }
+
+    cout << "\nTODAYS REMINDERS!" << endl;
+
+    while (fin.getline(title, 256)) {
+
+        fin.getline(description, 256);
+
+        fin.getline(done, 256);
+
+        fin.getline(hour, 256);
+
+        fin.getline(minute, 256);
+
+        fin.getline(month, 256);
+
+        fin.getline(day, 256);
+
+        fin.getline(year, 256);
+
+        if ((stoi(string(month)) == thisMonth) && (stoi(string(day)) == thisDay) && (stoi(string(year)) == thisYear)) {
+            cout << "Title -> " << string(title) << endl << "description-> "<< string(description) << endl << "stabilize : " << string(done) << endl;
         }
-    return Instance;
+
+        Event newEvent = Event(string(title), stoi(string(day)), MONTH(stoi(string(month))), stoi(string(year)), stoi(string(hour)), stoi(string(minute)), string(description) ,string(done));
+
+        container.insert(mmap_pair(title, newEvent));
+    }
+
+    fin.close();
+
+    Menu newMenu = Menu(container);
+
+    //menuChoice = 0;
+
+    //while (menuChoice != 4) {
+    newMenu.start();
+
+    //cout << menuChoice << endl;
+    //}
+
+    system("pause");
+    return 0;
 }
-
-
